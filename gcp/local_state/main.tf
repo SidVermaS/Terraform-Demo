cf0e63cff09d02c180f73da7aee087322c3e97b4
SidVermaS <sidvermas1234@gmail.com>
2024-10-24 21:53:47 +0530

    terraform for gcp to configure remote backend

--- gcp/local_state/main.tf
+++ gcp/local_state/main.tf
@@ -28,3 +28,12 @@ resource "google_storage_bucket" "terraform-storage-bucket" {
     name = "demo-terraform-storage-bucket"
     location = "IN"
 }
+
+# Configure Remote Backend
+# After the creation of the bucket, comment in & execute this block
+# terraform {
+#   backend "gcs" {
+#     bucket = "demo-terraform-storage-bucket"
+#     prefix = "terraform/state" # folder like structure (optional)
+#   }
+# }
